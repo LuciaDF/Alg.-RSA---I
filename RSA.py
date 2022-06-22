216ebe0d21ab370ddfbd6f7004656f559df55499
Karol525 <85766893+Karol525@users.noreply.github.com>
2022-06-22 15:15:13 -0500

    Create RSA.py

---
+++ RSA.py
@@ -0,0 +1,25 @@
+import resource
+
+def cifrar (a,b,c):
+    return resource.exp_Mod(a, b, c)
+
+def descifrar(a,b,c):
+    return resource.exp_Mod(a, b, c)
+
+p = resource.randomgen_primos(32)
+q = resource.randomgen_primos(32)
+n = p * q
+fi = resource.gen_fi(p,q)
+e = resource.gen_e(fi)
+d = resource.gen_d(e,fi)
+m = resource.randomgen_primos(32)
+
+print("p = ", p)
+print("q = ", q)
+print("n = ", n)
+print("ϕ(n) = ", fi)
+print("e = ", e)
+print ("d = ",d)
+
+print(cifrar(m,e,n))
+print(descifrar(m,d,n))
