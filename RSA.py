1ddf6a721400ef99c3d44e655fd6e147edbcabff
Lenovo <Lenovo@DESKTOP-3UFHVN0>
2022-06-22 15:07:36 -0500

    .

--- RSA.py
+++ RSA.py
@@ -5,21 +5,22 @@ def cifrar (a,b,c):
 def descifrar(a,b,c):
     return resource.exp_Mod(a, b, c)
 
+
 p = resource.randomgen_primos(32)
 q = resource.randomgen_primos(32)
 n = p * q
 fi = resource.gen_fi(p,q)
 e = resource.gen_e(fi)
 d = resource.gen_d(e,fi)
-m = resource.randomgen_primos(8)
+m = resource.randomgen_primos(32)
 
 print("p = ", p)
 print("q = ", q)
 print("n = ", n)
-print("fi = ", fi)
+print("ϕ(n) = ", fi)
 print("e = ", e)
 print ("d = ",d)
 
 print(cifrar(m,e,n))
-print(descifrar(m,e,n))
+print(descifrar(m,d,n))
 
